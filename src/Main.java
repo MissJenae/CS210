af3fc03de7b29840cd0b315840075455512241f4
MissJenae <jenae.a@live.com>
2023-09-18 13:44:05 -0700

    WellFormed Chapter 1 Exercise #3

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Exercise 3
+        System.out.println("A well-formed Java program has");
+        System.out.println("a main method with { and }");
+        System.out.println("braces.");
+        System.out.println("");
+        System.out.println("A System.out.println statement");
+        System.out.println("has ( and ) and usually a");
+        System.out.println("String that starts and ends");
+        System.out.println("with a \" character");
+        System.out.println("(But we type \\\" instead!)");
+    }
+}
